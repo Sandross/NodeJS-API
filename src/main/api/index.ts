c464585afc276623d101420abdcb73acbf9af5dc
Sandross <sandrosalasfilho3@gmail.com>
2023-06-17 09:42:08 -0300

    done typescript config

--- src/main/api/index.ts
+++
@@ -1,6 +0,0 @@
-import '../config/module-alias'
-import { PersonController } from '@/main/controllers';
-
-const p = new PersonController();
-console.log(p.speak('Sandro'));
-console.log(p.speak());
